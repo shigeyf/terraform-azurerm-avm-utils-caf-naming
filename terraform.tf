8689a8398368d5d15767e54f279daf2cd9ed02a8
Shige Fukushima <shigeyf@outlook.com>
2026-05-20 14:53:12 +0900

    Initial commit

---
+++ terraform.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_version = ">= 1.9, < 2.0"
+
+  required_providers {
+    azapi = {
+      source  = "Azure/azapi"
+      version = "~> 2.4"
+    }
+    # TODO: Ensure all required providers are listed here and the version property includes a constraint on the maximum major version.
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+    modtm = {
+      source  = "azure/modtm"
+      version = "~> 0.3"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.5"
+    }
+  }
+}
